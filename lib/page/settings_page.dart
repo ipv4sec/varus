4807d6cafafa8ff27424a7d2b202ae9a9a1a3fc3
ipv4sec <ipv4sec@globalsphere.tech>
2026-09-21 18:40:28 +0800

    feat: upgrade to Flutter 3.27 (Dart 3), AGP 8.6, modern dependencies

--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io' as io;
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:path_provider/path_provider.dart';
